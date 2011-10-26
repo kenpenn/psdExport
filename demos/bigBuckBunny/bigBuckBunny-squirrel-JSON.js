b58bcc026086b721c1c1a6a64341de87ba77de74
unknown <Ken@alienKen.(none)>
2011-10-25 17:41:51 -0700

    Added demos and readme

---
+++ demos/bigBuckBunny/bigBuckBunny-squirrel-JSON.js
@@ -0,0 +1,58 @@
+/* exported json for bigBuckBunny-squirrel.psd */
+
+
+var imgInput = function () {
+
+
+  var input = {
+    "container" : {
+      "width"    : 640,
+      "height"   : 363
+     },
+    "images" : [
+      {
+         "name"     : "bigBuckBunny-squirrel-4-sky",
+         "extension": ".png",
+         "left"     : 0,
+         "top"      : 0,
+         "width"    : 640,
+         "height"   : 363
+      },
+      {
+         "name"     : "bigBuckBunny-squirrel-3-cloud-left",
+         "extension": ".png",
+         "left"     : 0,
+         "top"      : 0,
+         "width"    : 296,
+         "height"   : 131
+      },
+      {
+         "name"     : "bigBuckBunny-squirrel-2-cloud-right",
+         "extension": ".png",
+         "left"     : 376,
+         "top"      : 155,
+         "width"    : 264,
+         "height"   : 208
+      },
+      {
+         "name"     : "bigBuckBunny-squirrel-1-tree",
+         "extension": ".png",
+         "left"     : 0,
+         "top"      : 25,
+         "width"    : 238,
+         "height"   : 338
+      },
+      {
+         "name"     : "bigBuckBunny-squirrel-0-squirrel",
+         "extension": ".png",
+         "left"     : 194,
+         "top"      : 66,
+         "width"    : 250,
+         "height"   : 279
+      }
+    ]
+  };
+
+
+  return JSON.stringify(input);
+}
